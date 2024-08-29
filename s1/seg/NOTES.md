aa330620456df16a95873f74a3c15b6e65f14ba4
Giovani Sant'Ana Pedroso <giovanisfpv@gmail.com>
2024-08-28 21:19:46 -0300

    generic

---
+++ s1/seg/NOTES.md
@@ -0,0 +1,12 @@
+# Conteudo P1
+
+- Slides Aula1
+- Slides Aula2 ate o 82
+- Slides Aula3
+
+## memorias primarias são:
+
+- volateis
+- caras
+- pequenas
+- rapidas
--- scripts/studie.sh
+++ scripts/studie.sh
@@ -9,13 +9,13 @@ if [ $DOW -eq 1 ]; then
 elif [ $DOW -eq 2 ]; then 
   cd ~/codes/fatec/s1/ter/ 
 elif [ $DOW -eq 3 ]; then 
-  cd ~/codes/fatec/s1/seg/ 
+  cd ~/codes/fatec/s1/qua/ 
 elif [ $DOW -eq 4 ]; then 
-  cd ~/codes/fatec/s1/seg/ 
+  cd ~/codes/fatec/s1/qui/ 
 elif [ $DOW -eq 5 ]; then 
-  cd ~/codes/fatec/s1/seg/ 
+  cd ~/codes/fatec/s1/sext/ 
 elif [ $DOW -eq 6 ]; then 
-  cd ~/codes/fatec/s1/seg/ 
+  cd ~/codes/fatec/s1/sab/ 
 fi
 
 mkdir $DAY
