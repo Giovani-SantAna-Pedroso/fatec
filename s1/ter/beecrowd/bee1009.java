74acb08f7c78782c9ac6bab08130bad9cb6d563b
Giovani Sant'Ana Pedroso <giovanisfpv@gmail.com>
2024-09-04 23:34:19 -0300

    some bee crowd challenges done

--- s1/ter/beecrowd/bee1009.java
+++ s1/ter/beecrowd/bee1009.java
@@ -1,4 +1,5 @@
 
+// Problem if rounding the numbers
 import java.lang.Math.*;
 import java.io.IOException;
 import java.util.Scanner;
@@ -12,19 +13,12 @@ public class bee1009 {
     double salary;
     double sales;
 
-    // name = input.next();
-    // salary = input.nextFloat();
-    // sales = input.nextFloat();
-    name = "manu";
-    // salary = 1700.00;
-    // sales = 1230.50;
-    salary = 500.00;
-    sales = 1230.30;
+    name = input.next();
+    salary = input.nextDouble();
+    sales = input.nextDouble();
 
-    String earns = String.format("%.7f", salary + (sales * 0.15));
+    double total = salary + (sales * 0.15);
 
-    System.out.printf("name: %s\n", name);
-    // System.out.printf("TOTAL = %.2f\n", String.valueOf(earns).substring(0, -2));
-    System.out.printf("TOTAL = %s\n", earns.substring(0, earns.length() - 1));
+    System.out.printf("TOTAL = R$ %.2f\n", total);
   }
 }
