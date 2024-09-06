9f4a3613d6c3be3dfd6e709993aff932f5b4024b
Giovani Sant'Ana Pedroso <giovanisfpv@gmail.com>
2024-09-05 21:25:05 -0300

    some bee crowd challenges done

---
+++ s1/ter/beecrowd/csharp/bee1006/Program.cs
@@ -0,0 +1,8 @@
+﻿double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+
+double averge = ((a * 2) + (b * 3) + (c * 5)) / 10;
+
+Console.WriteLine($"MEDIA = {averge:F1}");
+
